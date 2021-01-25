e30c0eb094c13eb3b64cf44cef59ee177fabff5a
Kevin williams <kevinwilliamsubk1@gmail.com>
2021-01-25 23:44:15 +0700

    Learn about Variable PHP

---
+++ Variable.php
@@ -0,0 +1,23 @@
+<?php
+$name = "Kevin";
+$age = 18;
+echo "Name : ";
+echo $name;
+echo "\n";
+
+echo "Age : ";
+echo $age;
+echo "\n";
+
+$contoh = "kevin";
+$$contoh = "Williams"; // The variable will turn into $kevin variable
+
+echo "Contoh : ";
+echo $contoh;
+echo "\n";
+
+echo "kevin : ";
+echo $kevin;
+
+
+
