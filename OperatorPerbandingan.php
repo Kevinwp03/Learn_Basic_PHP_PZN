450d3c07a52fd79fafa6239e203effccb7d504c7
Kevin Williams Permana <kevinity310@gmail.com>
2021-02-04 23:48:46 +0700

    //the last i have problem with push commit, but actuallu the problem in Name branch (github), and Repository setting (SourceTree)

---
+++ OperatorPerbandingan.php
@@ -0,0 +1,6 @@
+<?php
+var_dump("10" == 10); //nilai String akan berubah menjadi Integer (type jugling/perubahan tipe data secara otomatis)
+var_dump("10" === 10);
+
+var_dump(10 > 10);
+var_dump(9 < 10);
