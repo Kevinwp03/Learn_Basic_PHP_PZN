2adfc249603243148830e846f6db366b0319d5ae
Kevin Williams Permana <kevinity310@gmail.com>
2021-02-24 21:45:31 +0700

    Manipulation String - Konversi ke number atau sebaliknya

--- ManupulasiString.php
+++ ManupulasiString.php
@@ -1,6 +1,16 @@
 <?php
 
-$name = "Kevin Williams Permana";
+$fullName = "Kevin Williams Permana";
 
-echo "Name : " . $name . PHP_EOL;
+echo "Name : " . $fullName . PHP_EOL;
 echo "Value: " . 100 . "\n";
+
+$valueString = (string)100;
+var_dump($valueString);
+
+$valueInt = (int)"100";
+var_dump($valueInt);
+
+$valueFloat = (float)"10.10";
+var_dump($valueFloat);
+
