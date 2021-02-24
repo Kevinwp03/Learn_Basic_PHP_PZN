dbbc5753c45f66e3868e51a96685942bd5bc5fa6
Kevin Williams Permana <kevinity310@gmail.com>
2021-02-24 21:46:14 +0700

    Manipulation String - Mengakses karakter

--- ManupulasiString.php
+++ ManupulasiString.php
@@ -14,3 +14,10 @@ var_dump($valueInt);
 $valueFloat = (float)"10.10";
 var_dump($valueFloat);
 
+
+$name = "Kevin";
+echo $name[0]  . PHP_EOL;
+echo $name[1].PHP_EOL;
+echo $name[2].PHP_EOL;
+echo $name[3].PHP_EOL;
+echo $name[4].PHP_EOL;
