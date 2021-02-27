622cfa96e5dea5328e732a8a7423078558d69e2c
Kevin Williams Permana <kevinity310@gmail.com>
2021-02-27 16:58:26 +0700

    Learn For loop

---
+++ ForLoop.php
@@ -0,0 +1,9 @@
+<?php
+for($counter = 1; $counter <= 10; $counter++){
+    echo "Ini adalah For Loop dengan Kondisi ke-$counter ".PHP_EOL;
+}
+//Alternatve
+for($counter = 10; $counter >= 1; $counter--):
+    echo "Ini adalah For loop dengan Kondisi ke-$counter".PHP_EOL ;
+endfor;
+
