2002b4a7dcd2bdbd6aa227a0806f6e3d9caba5b3
Kevin Williams Permana <kevinity310@gmail.com>
2021-03-26 20:07:33 +0700

    Learn Arrow Function

---
+++ ArrowFunction.php
@@ -0,0 +1,11 @@
+<?php
+$firstName = "Kevin";
+$lastName = "Williams";
+
+$anonymousFunction = function() use($firstName, $lastName) : string{
+    return "Hello $firstName $lastName".PHP_EOL;
+};
+$arrowFunction = fn() => "Hello $firstName $lastName". PHP_EOL;
+
+echo $anonymousFunction();
+echo $arrowFunction();
